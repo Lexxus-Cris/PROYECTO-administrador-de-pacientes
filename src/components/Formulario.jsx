87e39feccae90581934d1c40d2121eb6509c6981
Uriel Cristino <urielcris.97@gmail.com>
2021-07-27 16:22:17 -0500

    Se agregan los campos del formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,10 +1,60 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 
 const Formulario = () => {
    return (
-      <div>
-         <h1>desde formulario</h1>
-      </div>
+      <Fragment>
+         <h2>Crear cita</h2>
+         <form action="">
+            <label htmlFor="">Nombre Mascota</label>
+            <input
+               type="text"
+               name="mascota"
+               alt="Nombre de la mascota"
+               placeholder="Nombre de la mascota"
+               className="u-full-width"
+            />
+            <label htmlFor="">Nombre Dueño</label>
+            <input
+               type="text"
+               name="propietario"
+               alt="Nombre del dueño"
+               placeholder="Nombre del dueño"
+               className="u-full-width"
+            />
+            <label htmlFor="">Número de contacto</label>
+            <input
+               type="tel"
+               name="phone"
+               alt="Número de contacto"
+               placeholder="Numero de contacto"
+               className="u-full-width"
+            />
+            <label htmlFor="">Fecha</label>
+            <input
+               type="date"
+               name="date"
+               alt="fecha de la cita"
+               className="u-full-width"
+            />
+            <label htmlFor="">Hora</label>
+            <input
+               type="time"
+               name="time"
+               alt="hora de la cita"
+               className="u-full-width"
+            />
+            <label htmlFor="">Sintomas</label>
+            <textarea 
+               name="sintomas"
+               cols="30"
+               rows="10"
+               placeholder="Agregar sintomas del pacience: vomito, diarrea, etc."
+               className="u-full-width"
+            >
+            </textarea>
+            <button type="submit" className="u-full-width button-primary">Agregar cita</button>
+         </form>
+      </Fragment>
    )
 }
 
