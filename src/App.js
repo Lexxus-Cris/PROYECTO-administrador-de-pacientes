6305f780a387ca07eed84c1eb29c86ed5b1365aa
Uriel Cristino <urielcris.97@gmail.com>
2021-07-27 15:48:47 -0500

    Configuracion de skeleton en el archivo App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import React from 'react'
+
+import Formulario from './components/Formulario.jsx';
+
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <h1>Administrador de pacientes</h1>
+      <div className="row">
+        <div className="one-half column">
+          <Formulario />
+        </div>
+        <div className="one-half column">
+          2
+        </div>
+      </div>
     </div>
   );
 }
